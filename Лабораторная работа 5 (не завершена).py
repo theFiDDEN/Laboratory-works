dab87ec2f86b87aebe9223b988dcdf5973a05fda
theFiDDEN <90830639+theFiDDEN@users.noreply.github.com>
2021-09-28 12:01:03 +0300

    Update Лабораторная работа 5 (не завершена).py

--- Лабораторная работа 5 (не завершена).py
+++ Лабораторная работа 5 (не завершена).py
@@ -17,6 +17,18 @@ def task3(A, B, C):
     print('Точка A =', A)
     print('Точка B =', B)
     print('Точка C =', C)
+    
+    if not a < c < b:
+        print('Точка C не расположена между точками A и B')
+        return 0
+    
     print('Длина отрезка AC =', abs(A - C))
     print('Длина отрезка BC =', abs(B - C))
     print('Произведение длин отрезков AC и BC =', abs(A - C) * abs(B - C))
+    
+def task4(x1, y1, x2, y2):
+    print('Координаты точек (', x1, ',', y1, ')', 'и', '(', x2, ',', y2, ')')
+    a = abs(x2 - x1)
+    b = abs(y2 - y1)
+    print('Периметр прямоугольника =', 2 * (a + b))
+    print('Площадь прямоугольника =', a * b)
